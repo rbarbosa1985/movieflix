84b44104af96d5e2484c1ebadec400d298b7f888
Roberto Barbosa <betoanselmo@gmail.com>
2021-03-26 22:36:05 -0300

    Details

--- frontend-web/src/pages/Admin/Movies/Catalog/MoviesDetails/index.tsx
+++ frontend-web/src/pages/Admin/Movies/Catalog/MoviesDetails/index.tsx
@@ -6,51 +6,43 @@ import { ParamsType } from './types';
 import { makePrivateRequest } from 'core/utils/request';
 import { ReactComponent as ArrowIcon } from 'core/assets/images/arrow.svg';
 import { Movie } from 'core/types/Movies';
-import MovieInfoLoader from '../Loaders/MovieInfoLoader';
-import MovieDescriptionLoader from '../Loaders/MovieDescriptionLoader';
+
 
 const MoviesDetails = () => {
      const { movieId } = useParams<ParamsType>();
      const [movie, setMovie] = useState<Movie>();
-     const [isLoading, setIsLoading] = useState(false);
 
      useEffect(() => {
-          setIsLoading(true);
           makePrivateRequest({ url: `/movies/${movieId}` })
                .then(response => setMovie(response.data))
-               .finally(() => setIsLoading(false));
      }, [movieId]);
 
      return (
-          <div className="product-details-container">
-               <div className="product-details card-base border-radius-20">
-                    <Link to="/admin" className="product-details-goback">
+          <div className="movie-details-container">
+               <div className="movie-details-card card-base border-radius-20">
+                    <Link to="/admin" className="movie-details-goback">
                          <ArrowIcon className="arrow-goback" />
                          <h1 className="text-goback">VOLTAR</h1>
                     </Link>
-                    <div className="row">
-                         <div className="col-6 pr-5">
-                              {isLoading ? <MovieInfoLoader /> : (
-                                   <>
-                                        <div className="product-details-card text-center">
-                                             <img src={movie?.imgUrl} alt={movie?.title} className="product-details-img" />
-                                        </div>
-                                        <h1 className="product-details-name">{movie?.title}</h1>
-                                   </>
-                              )}
-                         </div>
-                         <div className="col-6 product-details-card">
-                              {isLoading ? <MovieDescriptionLoader /> : (
-                                   <>
-                                        <h1 className="product-description-title">Descrição do Produto</h1>
-                                        <p className="product-description-text">
-                                             {movie?.synopsis}
-                                        </p>
-                                   </>
-                              )}
+                    <div className="row movie-detail">
+                         <img src={movie?.imgUrl} alt={movie?.title} className="movie-details-img" />
+                         <div className="movie-details-card2">
+                              <h1 className="movie-details-title">{movie?.title}</h1>
+                              <h1 className="movie-details-year">{movie?.year}</h1>
+                              <h1 className="movie-details-subtitle">{movie?.subTitle}</h1>
+                              <p className="movie-description-text">
+                                   {movie?.synopsis}
+                              </p>
                          </div>
                     </div>
                </div>
+               <div className="review-card card-base border-radius-20">
+                    <p>Oi</p>
+               </div>
+               <div className="review-user-card card-base border-radius-20"><Link to="/admin" className="movie-details-goback">
+                    <ArrowIcon className="arrow-goback" />
+                    <h1 className="text-goback">VOLTAR</h1>
+               </Link></div>
           </div>
      );
 };
